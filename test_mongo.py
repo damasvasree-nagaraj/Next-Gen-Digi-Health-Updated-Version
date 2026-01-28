f2e430aece93f2d90024dc414ee145670740aa70
Damasvasree <damasvasree@gmail.com>
2026-01-28 21:25:43 +0530

    Update test_mongo.py

--- test_mongo.py
+++ test_mongo.py
@@ -2,10 +2,11 @@ from pymongo import MongoClient
 import certifi
 
 client = MongoClient(
-    "mongodb+srv://nextgen_admin:nextgen123@cluster0.grbxsig.mongodb.net/?appName=Cluster0",
+    "mongodb+srv://YOUR_USERNAME:PASSWORD@cluster0.grbxsig.mongodb.net/?appName=Cluster0",
     tls=True,
     tlsCAFile=certifi.where(),
     serverSelectionTimeoutMS=5000
 )
 
 print(client.admin.command("ping"))
+
